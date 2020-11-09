b11205e0a4e3e0c0411ff57c3bb85346b592b622
laisk <laisk1@hotmail.com>
2020-11-09 13:51:12 +0200

    init commit

---
+++ src/main/java/tacos/web/HomeController.java
@@ -0,0 +1,16 @@
+package tacos.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
+
